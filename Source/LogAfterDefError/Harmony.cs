da44d5c9c567dca970fac1b88ec550f0f62857c5
newbie-forever <79868815+newbie-forever@users.noreply.github.com>
2024-07-25 22:09:58 +0800

    patch tracking

--- Source/LogAfterDefError/Harmony.cs
+++ Source/LogAfterDefError/Harmony.cs
@@ -4,6 +4,14 @@ using HarmonyLib;
 using UnityEngine;
 using Verse;
 using RimWorld;
+using System.Reflection.Emit;
+using System.Xml;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine.Assertions.Must;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace LogAfterDefError {
 
@@ -15,22 +23,161 @@ namespace LogAfterDefError {
 			Instance.PatchAll(Assembly.GetExecutingAssembly());
 		}
 
+		internal static void RemovePatches() {
+			Instance.UnpatchAll("ordpus.logafterdeferror");
+		}
+
+	}
+
+	[HarmonyPatch(typeof(LoadedModManager), nameof(LoadedModManager.ParseAndProcessXML))]
+	internal static class Verse__LoadedModManager__ParseAndProcessXML {
+		internal static bool Prepare() => LogAfterDefErrorModSettings.defTraceEnabled;
+		internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il) {
+			var list = instructions.ToList();
+			var contentMod = typeof(LoadableXmlAsset).Field(nameof(LoadableXmlAsset.mod));
+			var tryRegister = typeof(XmlInheritance).Method(nameof(XmlInheritance.TryRegister));
+			var localLog = il.DeclareLocal(typeof(LogMessage));
+			for(int i = 0; i < list.Count; ++i) {
+				var code = list[i];
+				if(code.Calls(tryRegister)) {	
+					yield return new CodeInstruction(OpCodes.Ldsfld, typeof(Log).Field(nameof(Log.messageQueue))).WithLabels(code.labels);
+					yield return new CodeInstruction(OpCodes.Ldfld, typeof(LogMessageQueue).Field(nameof(LogMessageQueue.lastMessage)));
+					yield return new CodeInstruction(OpCodes.Stloc_S, localLog);
+					code.labels = null;
+				}
+				yield return code;
+				if(code.Calls(tryRegister)) {
+					yield return new CodeInstruction(OpCodes.Ldloc_S, 5);
+					yield return new CodeInstruction(OpCodes.Ldloc_S, localLog);
+					yield return new CodeInstruction(OpCodes.Call, typeof(Verse__LoadedModManager__ParseAndProcessXML).Method(nameof(LoadPostfix)));
+				}
+			}
+		}
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static void LoadPostfix(LoadableXmlAsset xmlAsset, LogMessage __state) {
+			if(!LogAfterDefErrorModSettings.defTraceEnabled || Log.messageQueue.lastMessage == __state) return;
+			Log.Message($"[Def Error] {Utility.FormatAsset(xmlAsset)}");
+		}
+	}
+
+	[HarmonyPatch(typeof(XmlInheritance), nameof(XmlInheritance.TryRegisterAllFrom))]
+	internal static class Verse__XmlInheritance__TryRegisterAllFrom {
+		internal static bool Prepare() => LogAfterDefErrorModSettings.defTraceEnabled;
+		internal static void Prefix(ref LogMessage __state) {
+			__state = Log.messageQueue.lastMessage;
+		}
+
+		internal static void Postfix(LoadableXmlAsset xmlAsset, LogMessage __state) {
+			if(!LogAfterDefErrorModSettings.defTraceEnabled || Log.messageQueue.lastMessage == __state) return;
+			var defError = $"[Def Error] " + Utility.FormatAsset(xmlAsset);
+			Log.Message(defError);
+		}
 	}
 
 
 	[HarmonyPatch(typeof(DirectXmlLoader), nameof(DirectXmlLoader.DefFromNode))]
 	internal static class Verse__DirectXmlLoader__DefFromNode {
+		internal static bool Prepare() => LogAfterDefErrorModSettings.defTraceEnabled;
 
 		internal static void Prefix(ref LogMessage __state) {
 			__state = Log.messageQueue.lastMessage;
 		}
 
-		internal static void Postfix(Def __result, LoadableXmlAsset loadingAsset, LogMessage __state) {
-			if(__result == null || !LogAfterDefErrorModSettings.enabled || Log.messageQueue.lastMessage == __state) return;
-			var mod = loadingAsset?.mod ?? null;
+		internal static void Postfix(Def __result, XmlNode node, LoadableXmlAsset loadingAsset, LogMessage __state) {
+			if(__result == null || !LogAfterDefErrorModSettings.defTraceEnabled || Log.messageQueue.lastMessage == __state) return;
 			if(Log.messageCount > 900) Log.ResetMessageCount();
-			Log.Message("[Def Error] " + __result.defName + ":" + mod?.Name + loadingAsset?.FullFilePath?.Replace(mod?.RootDir, "") + "\n");
+			var defError = $"[Def Error] " + Utility.FormatAsset(__result.defName, loadingAsset);
+			if(LogAfterDefErrorModSettings.patchTraceEnabled) {
+				var operations = GetParentNodes(node, [node])
+					.Where(Utility.Operations.ContainsKey)
+					.SelectMany(x => Utility.Operations[x])
+					.GroupBy(Utility.Patches.ContainsKey)
+					.ToDictionary(k => k.Key, v => v.ToList());
+				if(operations.TryGetValue(true, out var list1) && !list1.EnumerableNullOrEmpty()) {
+					defError += "\nPossible Related Patches ::\n  " + string.Join("\n  ", 
+						list1.Select(x => Utility.FormatAsset(x.GetType().FullName, Utility.Patches[x])).Distinct());
+				}
+				if(operations.TryGetValue(false, out var list2) && !list2.EnumerableNullOrEmpty()) {
+					defError += "\nUnrecognized Patches ::\n  " + string.Join("\n  ", list2.Select(Utility.ToStringFull).Distinct());
+				}
+			}
+			Log.Message(defError);
+		}
+
+		internal static List<XmlNode> GetParentNodes(XmlNode node, List<XmlNode> result) {
+			if(XmlInheritance.resolvedNodes.TryGetValue(node, out var value) && value.parent != null) {
+				result.Add(value.parent.xmlNode);
+				GetParentNodes(value.parent.xmlNode, result);
+			}
+			return result;
 		}
 	}
 
+	[HarmonyPatch(typeof(Activator), nameof(Activator.CreateInstance), [typeof(Type)])]
+	internal static class System__Activator__CreateInstance {
+		internal static bool Prepare() => LogAfterDefErrorModSettings.patchTraceEnabled;
+		internal static void Postfix(Type? type, object __result) {
+			if(type == null || (__result is not PatchOperation operation)) return;
+			Utility.Patches[operation] = Utility.CurrentAsset.Value;
+		}
+	}
+
+	[HarmonyPatch(typeof(ModContentPack), nameof(ModContentPack.LoadPatches))]
+	internal static class Verse__ModContentPack__LoadPatches {
+		internal static bool Prepare() => LogAfterDefErrorModSettings.patchTraceEnabled;
+		internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			var getValue = typeof(List<LoadableXmlAsset>).PropertyGetter("Item");
+			var setValue = typeof(Verse__ModContentPack__LoadPatches).Method(nameof(SetPatch));
+			foreach(var code in instructions) {
+				yield return code;
+				if(code.Calls(getValue)) {
+					yield return new CodeInstruction(OpCodes.Dup);
+					yield return new CodeInstruction(OpCodes.Call, setValue);
+				}
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static void SetPatch(LoadableXmlAsset asset) => Utility.CurrentAsset.Value = asset;
+	}
+
+	[HarmonyPatch]
+	internal static class System__Xml__XmlNodeList__Item {
+
+		internal static bool Prepare() => LogAfterDefErrorModSettings.patchTraceEnabled;
+
+		internal static IEnumerable<MethodBase> TargetMethods() => AppDomain.CurrentDomain
+			.GetAssemblies()
+			.SelectMany(x => x.GetTypes())
+			.Where(x => x.IsSubclassOf(typeof(XmlNodeList)))
+			.Where(x => x.Method(nameof(XmlNodeList.Item)).DeclaringType == x)
+			.Select(x => x.Method(nameof(XmlNodeList.Item), [typeof(int)]));
+
+		internal static void Postfix(XmlNode __result) {
+			if(__result == null || Utility.PatchTracer.Value.Count == 0) return;
+			var node = __result;
+			while(node != null && node.ParentNode != null && node.ParentNode.Name != "Defs")
+				node = node.ParentNode;
+			if(node.ParentNode?.Name == "Defs") {
+				if(Utility.Operations.TryGetValue(node, out var value)) value.Add(Utility.PatchTracer.Value.Peek());
+				else Utility.Operations[node] = [Utility.PatchTracer.Value.Peek()];
+			}
+		}
+
+	}
+
+	[HarmonyPatch(typeof(PatchOperation), nameof(PatchOperation.Apply))]
+	internal static class Verse__PatchOperation__Apply {
+		internal static void Prefix(PatchOperation __instance) {
+			if(!Utility.Patches.ContainsKey(__instance) 
+				&& Utility.PatchTracer.Value.Count > 0) {
+				Utility.Patches[__instance] = Utility.Patches[Utility.PatchTracer.Value.Peek()];
+			}
+			Utility.PatchTracer.Value.Push(__instance);
+		}
+
+		internal static void Postfix() => Utility.PatchTracer.Value.Pop();
+	}
 }
--- Source/LogAfterDefError/LogAfterDefError.cs
+++ Source/LogAfterDefError/LogAfterDefError.cs
@@ -1,22 +1,59 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Xml;
 using UnityEngine;
 using Verse;
 
 namespace LogAfterDefError {
+
+	internal static class Utility {
+		internal static readonly Dictionary<PatchOperation, LoadableXmlAsset> Patches = [];
+		internal static readonly Dictionary<XmlNode, List<PatchOperation>> Operations = [];
+		internal static readonly ThreadLocal<LoadableXmlAsset> CurrentAsset = new();
+		internal static readonly ThreadLocal<Stack<PatchOperation>> PatchTracer = new(() => new Stack<PatchOperation>());
+		internal static string ToStringFull(this object obj) {
+			return obj.GetType().FullName + ":" + obj.GetType().GetRuntimeFields().Join(x => x.Name + "=" + x.GetValue(obj));
+		}
+
+		internal static string FormatAsset(LoadableXmlAsset asset) {
+			var mod = asset?.mod ?? null;
+			return $"{mod?.Name}{asset?.FullFilePath?.Replace(mod?.RootDir, "")}";
+		}
+
+		internal static string FormatAsset(string name, LoadableXmlAsset asset) {
+			return (name.NullOrEmpty() ? "" : name + ":") + FormatAsset(asset);
+		}
+	}
 
 	public class LogAfterDefErrorMod : Mod {
 
 		public LogAfterDefErrorMod(ModContentPack content) : base(content) {
 			GetSettings<LogAfterDefErrorModSettings>();
 			RuntimeHelpers.RunClassConstructor(typeof(HarmonyPatches).TypeHandle);
+			LongEventHandler.ExecuteWhenFinished(() => {
+				HarmonyPatches.RemovePatches();
+				Utility.Patches.Clear();
+				Utility.Operations.Clear();
+				Utility.CurrentAsset.Dispose();
+				Utility.PatchTracer.Dispose();
+			});
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect) {
 			var listing = new Listing_Standard();
 			listing.Begin(inRect);
-			listing.CheckboxLabeled("LogAfterDefError.Settings.Enabled".Translate(), ref LogAfterDefErrorModSettings.enabled);
+			listing.CheckboxLabeled("LogAfterDefError.Settings.DefTraceEnabled".Translate(), ref LogAfterDefErrorModSettings.defTraceEnabled);
+			if(LogAfterDefErrorModSettings.defTraceEnabled) {
+				listing.CheckboxLabeled("LogAfterDefError.Settings.PatchTraceEnabled".Translate(), ref LogAfterDefErrorModSettings.patchTraceEnabled);
+			}
 			listing.End();
 		}
 
@@ -27,10 +64,12 @@ namespace LogAfterDefError {
 
 	public class LogAfterDefErrorModSettings : ModSettings {
 
-		public static bool enabled = true;
+		public static bool defTraceEnabled = true;
+		public static bool patchTraceEnabled = false;
 
 		public override void ExposeData() {
-			Scribe_Values.Look(ref enabled, "enabled", true);
+			Scribe_Values.Look(ref defTraceEnabled, "defTraceEnabled", true);
+			Scribe_Values.Look(ref patchTraceEnabled, "patchTraceEnabled", false);
 		}
 
 	}
